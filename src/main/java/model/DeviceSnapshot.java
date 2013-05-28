f29dc30c48212ccf44308aa0b7f38a5d6bc2c255
Stefan Weghofer <st.weghofer@gmail.com>
2013-05-28 10:45:26 +0200

    Snapshot Model finished

--- src/main/java/model/DeviceSnapshot.java
+++ src/main/java/model/DeviceSnapshot.java
@@ -8,5 +8,114 @@ import com.google.code.morphia.annotations.Embedded;
  */
 @Embedded
 public class DeviceSnapshot {
+	
+	private String name;
+	private String description;
+	private String manufacturer;
+	private String driver;
+	
+	// do we need them?
+	private String freeField1;
+	private String freeField2;
+	private String freeField3;
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
+	/**
+	 * @return the manufacturer
+	 */
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	/**
+	 * @param manufacturer the manufacturer to set
+	 */
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	/**
+	 * @return the driver
+	 */
+	public String getDriver() {
+		return driver;
+	}
+
+	/**
+	 * @param driver the driver to set
+	 */
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	/**
+	 * @return the freeField1
+	 */
+	public String getFreeField1() {
+		return freeField1;
+	}
+
+	/**
+	 * @param freeField1 the freeField1 to set
+	 */
+	public void setFreeField1(String freeField1) {
+		this.freeField1 = freeField1;
+	}
+
+	/**
+	 * @return the freeField2
+	 */
+	public String getFreeField2() {
+		return freeField2;
+	}
+
+	/**
+	 * @param freeField2 the freeField2 to set
+	 */
+	public void setFreeField2(String freeField2) {
+		this.freeField2 = freeField2;
+	}
+
+	/**
+	 * @return the freeField3
+	 */
+	public String getFreeField3() {
+		return freeField3;
+	}
+
+	/**
+	 * @param freeField3 the freeField3 to set
+	 */
+	public void setFreeField3(String freeField3) {
+		this.freeField3 = freeField3;
+	}
+	
+	
 }
--- src/main/java/model/HardwareSnapshot.java
+++ src/main/java/model/HardwareSnapshot.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.code.morphia.annotations.Embedded;
 
 /**
@@ -8,5 +11,47 @@ import com.google.code.morphia.annotations.Embedded;
  */
 @Embedded
 public class HardwareSnapshot {
+	
+	@Embedded
+	private List<DeviceSnapshot> deviceSnapshots;
 
+	public HardwareSnapshot() {}
+	
+
+	/**
+	 * @return the deviceSnapshots
+	 */
+	public List<DeviceSnapshot> getDeviceSnapshots() {
+		return deviceSnapshots;
+	}
+
+	/**
+	 * @param deviceSnapshots the device snapshots to set
+	 */
+	public void setDeviceSnapshots(List<DeviceSnapshot> deviceSnapshots) {
+		this.deviceSnapshots = deviceSnapshots;
+	}
+	
+	/**
+	 * Adds a device snapshot to this hardware snapshot
+	 * @param deviceSnapshot the device snapshot to add
+	 */
+	public void addDeviceSnapshot(DeviceSnapshot deviceSnapshot) {
+		if(this.deviceSnapshots == null) {
+			this.deviceSnapshots = new ArrayList<DeviceSnapshot>();
+		}
+		if(!this.deviceSnapshots.contains(deviceSnapshot)){
+			this.deviceSnapshots.add(deviceSnapshot);
+		}
+	}
+
+	/**
+	 * Adds all device snapshots to this hardware snapshot
+	 * @param deviceSnapshots the snapshots to add
+	 */
+	public void addAllDeviceSnapshots(List<DeviceSnapshot> deviceSnapshots) {
+		for(DeviceSnapshot sn : deviceSnapshots){
+			this.addDeviceSnapshot(sn);
+		}
+	}
 }
