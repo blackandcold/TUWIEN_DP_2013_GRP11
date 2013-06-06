4285dc73d58fd834ee1ac78edc95766ee6069341
Johannes Kurz <j.kurz@gmx.at>
2013-06-06 12:30:29 +0200

    hw inventory

--- src/main/java/recorder/windows/WindowsRecorder.java
+++ src/main/java/recorder/windows/WindowsRecorder.java
@@ -1,58 +1,110 @@
 package recorder.windows;
 
-import java.net.UnknownHostException;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.DeviceSnapshot;
 import model.HardwareSnapshot;
-
-import org.jinterop.dcom.common.JIException;
-import org.jinterop.dcom.core.JIComServer;
-import org.jinterop.dcom.core.JIProgId;
-import org.jinterop.dcom.core.JISession;
-
 import recorder.AbstractRecorder;
 /**
  * This class performs the inventory service for the Microsoft Windows operating system.
- * @author Stefan Weghofer
+ * @author Johannes Kurz
  */
 public class WindowsRecorder 
 	extends AbstractRecorder {
 
 	@Override
-	protected HardwareSnapshot performHardwareInventory() {
-		/*
-            String os = System.getProperty("s.name").toLowerCase();
-            try 
-            {   
-                if(os.contains("win"))
-                {
-                    return windowsSystemInventory();
-                }
-                else if(os.contains("mac"))
-                {
-                    // TODO mac specific HW
-                }
-                else if(os.contains("nix")|| os.contains("nux"))
-                {
-                    // TODO unix specific HW
-                }
-            } catch (JIException ex) {
-                Logger.getLogger(WindowsRecorder.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (UnknownHostException ex) {
-                Logger.getLogger(WindowsRecorder.class.getName()).log(Level.SEVERE, null, ex);
-            }*/
-            return new HardwareSnapshot();
- 	}
-
-        private HardwareSnapshot windowsSystemInventory() throws JIException, UnknownHostException
+	protected HardwareSnapshot performHardwareInventory() 
         {
-            JISession session = JISession.createSession("localhost","wmitest","");
-
-            // TODO 
-            JIComServer comServer = new JIComServer(JIProgId.valueOf("WMI.Application"),"127.0.0.1",session);
-
-            JISession.destroySession(session);
-
-            return null;
-
+			DeviceSnapshot tmpSnapshot = new DeviceSnapshot();
+			HardwareSnapshot hwSnapshot = new HardwareSnapshot();
+			
+			tmpSnapshot = generateDeviceSnapshot("Memory");
+			hwSnapshot.addDeviceSnapshot(tmpSnapshot);
+			
+			tmpSnapshot = generateDeviceSnapshot("BIOS");
+			hwSnapshot.addDeviceSnapshot(tmpSnapshot);
+			
+			tmpSnapshot = generateDeviceSnapshot("Mainboard");
+			hwSnapshot.addDeviceSnapshot(tmpSnapshot);
+			
+            return hwSnapshot;
         }
+	
+		private DeviceSnapshot generateDeviceSnapshot(String hardWareName)
+		{
+			DeviceSnapshot tmpSnapshot = new DeviceSnapshot();
+			String value;
+			
+			try 
+			{
+				switch(hardWareName)
+				{
+				case "Memory":
+					value = jWMI.getWMIValue("SELECT * FROM Win32_ComputerSystem", "TotalPhysicalMemory");
+					tmpSnapshot.addProperty("TotalPhysicalMemory", value);
+					tmpSnapshot.setName("Memory");
+					
+					value = jWMI.getWMIValue("SELECT * FROM Win32_PhysicalMemory", "Description");
+					tmpSnapshot.setDescription(value);
+					
+					value = jWMI.getWMIValue("SELECT * FROM Win32_PhysicalMemory", "FormFactor");
+					tmpSnapshot.addProperty("FormFactor", value);
+					
+					value = jWMI.getWMIValue("SELECT * FROM Win32_PhysicalMemory", "Speed");
+					tmpSnapshot.addProperty("Speed", value);
+					
+					
+					tmpSnapshot.setName("Memory");
+					break;
+					
+				case "BIOS":
+					value = jWMI.getWMIValue("SELECT * FROM Win32_BIOS", "BIOSVersion");
+					tmpSnapshot.addProperty("BIOSVersion", value);
+					
+					value = jWMI.getWMIValue("SELECT * FROM Win32_BIOS", "Caption");
+					tmpSnapshot.setName(value);
+					
+					value = jWMI.getWMIValue("SELECT * FROM Win32_MemoryArray", "DeviceID");
+					tmpSnapshot.setDescription(value);
+					value = jWMI.getWMIValue("SELECT * FROM Win32_MemoryArray", "DeviceID");
+					tmpSnapshot.setManufacturer(value);
+					
+					value = jWMI.getWMIValue("SELECT * FROM Win32_MemoryArray", "SerialNumber");
+					tmpSnapshot.addProperty("SerialNumber", value);
+					
+					value = jWMI.getWMIValue("SELECT * FROM Win32_MemoryArray", "SMBIOSBIOSVersion");
+					tmpSnapshot.setDriver(value);
+					
+					value = jWMI.getWMIValue("SELECT * FROM Win32_BIOS", "ReleaseDate");
+					tmpSnapshot.addProperty("ReleaseDate", value);
+					
+					break;
+					
+				case "Mainboard":
+					value = jWMI.getWMIValue("SELECT * FROM Win32_MotherboardDevice", "Description");
+					tmpSnapshot.addProperty("Description", value);
+					value = jWMI.getWMIValue("SELECT * FROM Win32_MotherboardDevice", "Name");
+					tmpSnapshot.setName(value);
+					value = jWMI.getWMIValue("SELECT * FROM Win32_MotherboardDevice", "PrimaryBusType");
+					tmpSnapshot.addProperty("PrimaryBusType", value);
+					
+					
+					break;
+				}
+			
+
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            return tmpSnapshot;
+			
+		}
 }
